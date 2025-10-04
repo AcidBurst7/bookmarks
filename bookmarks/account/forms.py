197f0bdc05d94b80d2d9e223710d78d333939cd2
varak <varaksinaalla@mail.ru>
2025-10-04 13:41:11 +0300

    edit profile and password

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
 
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
@@ -21,3 +23,14 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError('Пароли не совпадают!')
         return cd['password2']
 
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['date_of_birth', 'photo']
--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,8 +1,15 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render
-from .forms import LoginForm, UserRegistrationForm
+from .forms import (
+    LoginForm, 
+    UserRegistrationForm,
+    UserEditForm,
+    ProfileEditForm
+)
+from .models import Profile
 
 
 @login_required
@@ -50,6 +57,7 @@ def register(request):
                 form.cleaned_data['password']
             )
             new_user.save()
+            Profile.objects.create(user=new_user)
             return render(
                 request,
                 'account/register_done.html',
@@ -62,3 +70,39 @@ def register(request):
                 'account/register.html',
                 {'form': form}
             )
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(
+            instance=request.user, 
+            data=request.POST
+        )
+
+        profile_form = ProfileEditForm(
+            instance=request.user.profile,
+            data=request.POST, 
+            files=request.FILES
+        )
+            
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+        return render(
+                request, 
+                'account/dashboard.html',
+                {'section': 'dashboard'}
+            )
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+
+    return render(
+        request,
+        'account/edit.html',
+        {
+            'user_form': user_form,
+            'profile_form': profile_form
+        }
+    )
